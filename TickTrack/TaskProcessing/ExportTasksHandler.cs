715d77f6ad9d488cb69806af144509cafe9a72af
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-04-04 06:07:05 +0200

    Improve project file structure

--- TickTrack/TaskProcessing/ExportTasksHandler.cs
+++ TickTrack/TaskProcessing/ExportTasksHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TickTrack.Business_logic
+namespace TickTrack.TaskProcessing
 {
     class ExportTasksHandler
     {
