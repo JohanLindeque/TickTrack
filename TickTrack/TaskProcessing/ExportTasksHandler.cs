0f36c15ff17c8a57968eea873e6357e2982a3856
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2025-04-04 06:35:17 +0200

    Implement checks if file and folder exists

--- TickTrack/TaskProcessing/ExportTasksHandler.cs
+++ TickTrack/TaskProcessing/ExportTasksHandler.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TickTrack.Models;
 
 namespace TickTrack.TaskProcessing
 {
     class ExportTasksHandler
     {
+        public void ExportToText(TaskEntryModel tasks, string filePath)
+        {
+
+        }
+
+        public void ExportToMarkdown(TaskEntryModel tasks, string filePath)
+        {
+
+        }
+
+        //public void ExportToJson(TaskEntryModel tasks, string filePath)
+        //{
+
+        //}
     }
 }
